7b77846443603b4b80f9335fc1fa1d152036b24a
Raul F Dias <raulfdias.ti@gmail.com>
2021-01-19 21:06:57 -0300

    init

--- src/app/controllers/API/AlunoAPIController.js
+++ src/app/controllers/API/AlunoAPIController.js
@@ -3,7 +3,23 @@ class AlunoAPIController {
         console.log(req)
     }
 
+    search(req, res) {
+        console.log(req)
+    }
+
+    show(req, res) {
+        console.log(req)
+    }
+
     store(req, res) {
+        console.log(req)
+    }
+
+    update(req, res) {
+        console.log(req)
+    }
+
+    delete(req, res) {
         console.log(req)
     }
 }
