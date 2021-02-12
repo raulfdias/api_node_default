0b8a076fd94d8005c34968b15177f4b4851454ce
Raul F Dias <raulfdias.ti@gmail.com>
2021-02-12 19:46:11 -0300

    Ajuste de configurações

--- src/config/database.js
+++ src/config/database.js
@@ -3,55 +3,58 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve('src', '.env') });
 
 const connections = {
-    "dev": {
-        "dialect": 'mysql',
-        "host": process.env.DB_HOST || '127.0.0.1',
-        "port": process.env.DB_PORT || 3306,
-        "database": process.env.DB_DATABASE || 'db_app_node',
-        "username": process.env.DB_USERNAME || 'root',
-        "password": process.env.DB_PASSWORD || '',
-        "operatorAliases": false,
-        "timezone": '-03:00',
-        "define": {
-            "charset": 'utf8',
-            "collate": 'utf8_general_ci',
-            "timestamps": true,
-            "underscored": true,
-            "underscoredAll": true
+    dev: {
+        dialect: 'mysql',
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT || 3306,
+        database: process.env.DB_DATABASE || 'db_app_node',
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '',
+        operatorAliases: false,
+        timezone: '-03:00',
+        logging: console.log,
+        define: {
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
         }
     },
-    "hml": {
-        "dialect": 'mysql',
-        "host": process.env.DB_HOST || '127.0.0.1',
-        "port": process.env.DB_PORT || 3306,
-        "database": process.env.DB_DATABASE || 'db_app_node',
-        "username": process.env.DB_USERNAME || 'root',
-        "password": process.env.DB_PASSWORD || '',
-        "operatorAliases": false,
-        "timezone": '-03:00',
-        "define": {
-            "charset": 'utf8',
-            "collate": 'utf8_general_ci',
-            "timestamps": true,
-            "underscored": true,
-            "underscoredAll": true
+    hml: {
+        dialect: 'mysql',
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT || 3306,
+        database: process.env.DB_DATABASE || 'db_app_node',
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '',
+        operatorAliases: console.log,
+        timezone: '-03:00',
+        logging: true,
+        define: {
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
         }
     },
-    "prd": {
-        "dialect": 'mysql',
-        "host": process.env.DB_HOST || '127.0.0.1',
-        "port": process.env.DB_PORT || 3306,
-        "database": process.env.DB_DATABASE || 'db_app_node',
-        "username": process.env.DB_USERNAME || 'root',
-        "password": process.env.DB_PASSWORD || '',
-        "operatorAliases": false,
-        "timezone": '-03:00',
-        "define": {
-            "charset": 'utf8',
-            "collate": 'utf8_general_ci',
-            "timestamps": true,
-            "underscored": true,
-            "underscoredAll": true
+    prd: {
+        dialect: 'mysql',
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT || 3306,
+        database: process.env.DB_DATABASE || 'db_app_node',
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '',
+        operatorAliases: false,
+        timezone: '-03:00',
+        logging: false,
+        define: {
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
         }
     }
 };
